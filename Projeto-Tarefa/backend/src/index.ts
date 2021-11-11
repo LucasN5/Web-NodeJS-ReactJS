05ef235bdd4c411b95fec79566375f1c4dca8e61
Figurante3 <lucasxben@hotmail.com>
2021-11-11 17:33:56 -0300

    Atualização 2

--- Projeto-Tarefa/backend/src/index.ts
+++ Projeto-Tarefa/backend/src/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as cors from 'cors';
 import routes from "./routes";
 
 
@@ -9,6 +10,7 @@ import routes from "./routes";
 const app = express()
 createConnection()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(routes)
 
